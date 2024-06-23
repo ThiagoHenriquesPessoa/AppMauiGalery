545df24b48564dba0b9e2da0c5f09f92a1923cf4
ThiagoHenriquesPessoa <thiagokxl5@gmail.com>
2024-06-23 13:29:24 -0300

    Adicionar arquivos de projeto.

---
+++ AppMauiGalery/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppMauiGalery
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
